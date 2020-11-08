aa3ac352f148bcf34206b0b38711d89dc5e57b77
Liliia Novikova <kplily85@gmail.com>
2020-11-08 19:30:24 +0200

    created API, template. styles

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
 import "./css/common.css";
-import apiService from './js/apiService.js'
+import ImgApiService from './js/apiService.js'
+import refs from './js/refs.js'
+import templateCards from './template/template.hbs'
+
+const imgApiService = new ImgApiService();
+
+
+refs.form.addEventListener('submit', onSearch)
+refs.loadMoreBth.addEventListener('click', onLoadMore)
+
+
+
+function onSearch(evt) {
+    evt.preventDefault()
+    
+  imgApiService.query = evt.currentTarget.elements.query.value;
+  console.log(imgApiService.query);
+  imgApiService.resetPage();
+  imgApiService.fetchImg().then(renderImgCard);
+}
+
+function onLoadMore() {
+  imgApiService.fetchImg().then(renderImgCard);  
+}
+
+function renderImgCard(hits) {
+  const markupImgCard = templateCards(hits)
+  console.log(hits);
+    refs.galeryList.insertAdjacentHTML('beforeend', markupImgCard)
+}
---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+const refs = {
+    btn: document.querySelector('.js-btn'),
+    form: document.querySelector('#search-form'),
+    loadMoreBth: document.querySelector('[data-action="load-more"]'),
+    galeryList: document.querySelector('.gallery')
+}
+
+export default refs
